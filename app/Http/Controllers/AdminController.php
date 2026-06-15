4be9fde934edca712295b96f78577d99e63d629d
Daria <laytlllesore@gmail.com>
2026-06-15 16:33:12 +0500

    Init project

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Appointment;
+use App\Models\Review;
+use App\Models\User;
+class AdminController extends Controller
+{
+    //
+}
---
+++ app/Models/Review.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Appointment;
+use App\Models\Review;
+use App\Models\User;
+class Review extends Model
+{
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+    public function(){
+        return $this->(::class),
+    }
+}
